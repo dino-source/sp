d6022dc390a013f57e3b7391e2c225ebe5fe8600
Pesna <prostopesna74@gmail.com>
2023-07-02 21:25:20 +0400

    Solve problem 'display username and address'

--- chapter02/exercises/ex01/main.cpp
+++ chapter02/exercises/ex01/main.cpp
@@ -4,3 +4,13 @@
 
 // Write a C++ program that displays your name and address (or if you value your
 // privacy, a fictitious name and address).
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string address = "Gantiadi, 24";
+    std::string name = "Nadia Sitkareva";
+    char delimiter = '\n';
+    std::cout << name << delimiter << address << delimiter;
+}
