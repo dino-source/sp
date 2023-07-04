7d993f9756d87d5bf4725bff0c08a05518965acf
Pesna <prostopesna74@gmail.com>
2023-07-04 09:45:50 +0400

    Small program to convert distance from kilometers to yards.

--- chapter02/exercises/ex02/main.cpp
+++ chapter02/exercises/ex02/main.cpp
@@ -4,3 +4,19 @@
 
 // Write a C++ program that asks for a distance in furlongs and converts it to yards.
 // (One furlong is 220 yards.)
+
+#include <iostream>
+
+int main()
+{
+    int distance; 
+    char delimiter = '\n';
+
+    std::cout << "Please, enter distance in furlongs." << delimiter;
+    std::cin >> distance;
+    distance = distance * 220;
+    std::cout << "Tnen it's " << distance << " yards." <<delimiter;
+    
+    return 0;
+
+}
