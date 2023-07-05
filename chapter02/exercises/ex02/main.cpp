ae634a4f976454e0c52570016e1a2ee6eeb5e57e
Pesna <prostopesna74@gmail.com>
2023-07-05 18:45:39 +0400

    Refactor Ex02

--- chapter02/exercises/ex02/main.cpp
+++ chapter02/exercises/ex02/main.cpp
@@ -9,13 +9,15 @@
 
 int main()
 {
-    int distance; 
+    constexpr int YARDS_IN_FURLONG = 220;
+    int distance_in_furlongs; 
     char delimiter = '\n';
 
     std::cout << "Please, enter distance in furlongs." << delimiter;
-    std::cin >> distance;
-    distance = distance * 220;
-    std::cout << "Tnen it's " << distance << " yards." <<delimiter;
+    std::cin >> distance_in_furlongs;
+    // distance_in_furlongs *= YARDS_IN_FURLONG;
+    distance_in_furlongs = distance_in_furlongs * YARDS_IN_FURLONG;
+    std::cout << "Tnen it's " << distance_in_furlongs << " yards." << delimiter;
     
     return 0;
 
