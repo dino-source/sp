53be5eac2cf4af4d27f573ac16069b40a4d0c72b
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-07-06 23:09:07 +0500

    Roll back repo to initial state and add 'Hello world'

--- chapter02/exercises/ex02/main.cpp
+++ chapter02/exercises/ex02/main.cpp
@@ -4,21 +4,3 @@
 
 // Write a C++ program that asks for a distance in furlongs and converts it to yards.
 // (One furlong is 220 yards.)
-
-#include <iostream>
-
-int main()
-{
-    constexpr int YARDS_IN_FURLONG = 220;
-    int distance_in_furlongs; 
-    char delimiter = '\n';
-
-    std::cout << "Please, enter distance in furlongs." << delimiter;
-    std::cin >> distance_in_furlongs;
-    // distance_in_furlongs *= YARDS_IN_FURLONG;
-    distance_in_furlongs = distance_in_furlongs * YARDS_IN_FURLONG;
-    std::cout << "Tnen it's " << distance_in_furlongs << " yards." << delimiter;
-    
-    return 0;
-
-}
--- chapter02/exercises/ex04/main.cpp
+++ chapter02/exercises/ex04/main.cpp
@@ -7,20 +7,3 @@
 // Enter your age: 29
 // Your age in months is 384.
 
-
-#include <iostream>
-
-int main ()
-{
-    constexpr int MONTHS_IN_YEAR = 12;
-    int age_in_years;
-    int age_in_months;
-    char delimiter = '\n';
-
-    std::cout << "Please, enter your age in years:" << delimiter;
-    std::cin >> age_in_years;
-    age_in_months = age_in_years * MONTHS_IN_YEAR;
-    std::cout << "Your age in months is " << age_in_months << " months." << delimiter;
-
-    return 0;
-}
