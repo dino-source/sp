5e8bc5caa621f8621ec084f5c2baadd4df2184c5
merlin <merlin2z@bk.ru>
2023-07-13 21:34:42 +0400

    Add std::cin usage example

--- online_sessions/2023-07-13/main.cpp
+++ online_sessions/2023-07-13/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 
 
 int main()
 {
     std::cout << "Please enter your name: ";
-
+    std::string username;
+    std::cin >> username;
+    std::cout << "Yay! Now I know your name! The name is " << username << "\n"; 
 }
