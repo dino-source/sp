c8834fffcdc8f92bd03d268004b6c92ee4c0f0c5
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-07-13 21:19:26 +0500

    Solve problem ex02

--- chapter02/exercises/ex02/main.cpp
+++ chapter02/exercises/ex02/main.cpp
@@ -3,5 +3,21 @@
 // Chapter 2. Exercise 2.
 
 // Write a C++ program that asks for a distance in furlongs and converts it to yards.
-
 // (One furlong is 220 yards.)
+
+#include <iostream>
+
+int main()
+{
+    int distance_in_furlongs;
+    int yards_in_furlongs = 220;
+    char delimiter = '\n';
+
+    std::cout << "Please enter distance in furlongs:\n";
+    std::cin >> distance_in_furlongs;
+    std::cout << '\n';
+    int distance_in_yards = distance_in_furlongs * yards_in_furlongs;
+    std::cout << "Then it's " << distance_in_yards << " yards in furlongs" << '\n';
+
+    return 0;
+}
