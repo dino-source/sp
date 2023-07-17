4bef92631ef1962ae747bc21c5650a174ec29f2e
Pesna <prostopesna74@gmail.com>
2023-07-17 21:42:37 +0400

    Add namespaces (non working draft)

--- online_sessions/2023-07-17/main.cpp
+++ online_sessions/2023-07-17/main.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
 #include <string>
-
-void output(std::string const &message, char delim = '\n');
-void output(int number, char delim = '\n');
-int request_integer_from_user(int);
-
-void output(std::string const &message, char delim = '\n')
+namespace nadia
 {
-    std::cout << message << delim;
+    void output(std::string const &message, bool dummy)
+    {
+        std::cout << message;
+    }
+    
+    void output(int number, bool dummy)
+    {
+        std::cout << number;
+    }
+    
+    int request_integer_from_user()
+    {
+        int integer_from_user;
+        std::cin >> integer_from_user;
+        return integer_from_user;
+    } 
+    
 }
 
-void output(int number, char delim = '\n')
+namespace dima
 {
-    std::cout << number << delim;
+    void output(std::string const &message, char delim = '\n');
+    void output(int number, char delim = '\n');
+
+
+    void output(std::string const &message, char delim = '\n')
+    {
+        std::cout << message << delim;
+    }
+
+    void output(int number, char delim = '\n')
+    {
+        std::cout << number << delim;
+    }
 }
-
-
-int request_integer_from_user(int)
-{
-    int integer_from_user;
-    std::cin >> integer_from_user;
-    return integer_from_user;
-}
-
 
 
 int main()
 {
-
+    nadia::output("Please, enter an integer.\n", true);
+    int x = nadia::request_integer_from_user();
+    nadia::output(x, true);
 }
