62eea6fb494954a51e40fcc49031e465fdab9de1
Pesna <prostopesna74@gmail.com>
2023-07-17 19:54:04 +0400

    Solve task ex07 (time output - naive implementation)

--- chapter02/exercises/ex07/main.cpp
+++ chapter02/exercises/ex07/main.cpp
@@ -8,3 +8,30 @@
 // Enter the number of hours: 9
 // Enter the number of minutes: 28
 // Time: 9:28
+
+#include <iostream>
+
+void hours(int);
+void minutes(int);
+
+int main ()
+{
+    int hours(9);  
+    std::cout << "Enter the number of hours: ";
+    int count_hours;
+    std::cin >> count_hours;
+    int minutes(28);  
+    std::cout << "Enter the number of minutes: ";
+    int count_minutes;
+    std::cin >> count_minutes;
+    std::cout << "Time: " << count_hours <<':' << count_minutes << '\n';
+
+    return 0;
+}
+
+void simon(int n)   // define the simon() function
+{
+    using namespace std;
+
+    cout << "Simon says touch your toes " << n << " times." << endl; 
+}
