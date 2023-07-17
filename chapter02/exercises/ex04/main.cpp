c218a781ade48f9fe17221c22ff05571db5c345b
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-07-17 07:20:12 +0500

    Solve problem ex04

--- chapter02/exercises/ex04/main.cpp
+++ chapter02/exercises/ex04/main.cpp
@@ -7,3 +7,19 @@
 // Enter your age: 29
 // Your age in months is 384.
 
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int months_in_a_year = 12;
+    int age_in_years;
+    int age_in_months;
+    char delimiter = '\n';
+    cout << "Please, enter your age:";
+    cin >> age_in_years;
+    cout << '\n';
+    age_in_months = age_in_years * months_in_a_year;
+    cout << "Your age in months is " << age_in_months;
+    return 0; 
+}
