c8f5edb0877bd36c5f28ef8dd0bafd354f8a702d
merlin <merlin2z@bk.ru>
2023-07-17 19:58:53 +0400

    Solve problem ex01

--- chapter02/exercises/ex01/main.cpp
+++ chapter02/exercises/ex01/main.cpp
@@ -4,3 +4,12 @@
 
 // Write a C++ program that displays your name and address (or if you value your
 // privacy, a fictitious name and address).
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Dmitriy V merlin2z@bk.ru\n";
+    return 0;
+}
+
