f530c91a05162b154cff1a8b1be29024fd76ae7f
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-07-17 06:16:05 +0500

    Solve problem ex03

--- chapter02/exercises/ex03/main.cpp
+++ chapter02/exercises/ex03/main.cpp
@@ -10,3 +10,33 @@
 // See how they run
 // One function, called two times, should produce the first two lines, and the remain-
 // ing function, also called twice, should produce the remaining output.
+
+#include <iostream>
+
+void message_about_three_blind_mice(); // function prototype
+void message_about_see_how_they_run(); // function prototype
+
+void three_blind_mice()
+{
+    using namespace std;
+   
+    cout << "Three blind mice\n";
+}
+
+void see_how_they_run()
+{
+    using namespace std;
+
+    cout << "See how they run\n";
+}
+
+int main()
+{
+    using namespace std;
+    cout << "Three blind mice\n";
+    cout << "Three blind mice\n";
+    cout << "See how they run\n";
+    cout << "See how they run\n";
+    
+    return 0;
+}
