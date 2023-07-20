6525d8849a11f6d3a9fbab008fd593b1f3e4e906
Pesna <prostopesna74@gmail.com>
2023-07-20 20:25:26 +0400

    Add

---
+++ online_sessions/2023-07-20/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+struct Student
+{
+    std::string first_name {"John"};
+    std::string last_name {"Doe"};
+    std::string id {"1234-5678"};
+    std::string mobile_phone {"+1234567890"};
+    std::string address {"Seattle WA, 92 NW st., apt. 11"};
+    std::string email {"noname@anonymous.us"};
+    int age {18};
+    int grade {1};
+    void introduce_yourself("Please, intoduce your")
+};
+
+int main()
+{
+
+}
