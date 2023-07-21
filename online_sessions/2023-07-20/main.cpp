0b313b72ccc6f62e167cfb4517808879ff45abde
Dino Source <dinosource.cpp@gmail.cpp>
2023-07-21 23:26:14 +0400

    Add std::erase_if + lambda expression usage example

--- online_sessions/2023-07-20/main.cpp
+++ online_sessions/2023-07-20/main.cpp
@@ -2,22 +2,26 @@
 #include <string>
 #include <vector>
 
+
 struct Student
 {
     std::string first_name {"John"};
     std::string last_name {"Doe"};
-    std::string id {"1234-5678"};
-    std::string mobile_phone {"+1234567890"};
+    std::string id {"A-0000-0000"};
+    std::string mobile_phone {"++1(253)632-32-32"};
     std::string address {"Seattle WA, 92 NW st., apt. 11"};
     std::string email {"noname@anonymous.us"};
     int age {18};
     int grade {1};
     double health {100.0};
 
-    void introduce_yourself() const
+    void introduce_yourself(bool report_age = true, std::string const &delim = "\n") const
     {
-        std::cout << "My name is " << first_name << ' ' << last_name << '\n';
-        std::cout << "I am " << age << " years old.\n";
+        std::cout << "My name is " << first_name << ' ' << last_name << '.';
+        if (report_age) {
+            std::cout << " I am " << age << " years old.";            
+        }
+        std::cout << delim;
     }
     
     void confess() const
@@ -27,47 +31,112 @@ struct Student
 
     void drink(int shots)
     {
-        health /= shots;
+        health /= shots; // health = health / shots;
     }
 };
 
-void live(std::vector<Student> &students)
-{
-    for (auto &student : students)
-    {
-        student.drink(2);
-    }
-} 
+using Students = std::vector<Student>;
 
-void print(std::vector<Student> const &students) 
+void print(Students const &students, std::string const &delim = "\n\n") 
 {
     for (auto const &student : students)
     {
         student.introduce_yourself();
-    }   
+    }
+    std::cout << delim;
 }
 
+void print(Student const &student, std::string const &delim = "\n") 
+{
+    student.introduce_yourself();
+    std::cout << delim;
+}
+
+void live(Students &students)
+{
+    static bool one_of_students_is_dead = false;
+    if (!one_of_students_is_dead)
+    {
+        for (auto &s : students)
+        {
+            s.drink(2);
+            s.introduce_yourself(false, " ");
+            s.confess();
+
+            if (s.health < 1)
+            {
+                std::cout << s.first_name << ' ' << s.last_name << " is dead.\n\n\n\n";
+                one_of_students_is_dead = true;
+                std::erase_if(students, [](Student &s){ return s.health < 1; });
+                return;
+            }
+        }
+        std::cout << "\n";        
+    }
+} 
 
 int main()
 {
-    Student george;
-    george.first_name = "George";
-    Student nataly;
-    nataly.first_name = "Nataly";
-    nataly.last_name = "Rostova";
-    nataly.age = 19;
-    Student robert;
-    robert.first_name = "Robert";
-    robert.age = 20;
+    Student george
+    {
+        .first_name = "George",
+        .last_name = "Blake",
+        .id = "A-0000-0001",
+        .mobile_phone = "+1(253)632-32-32",
+        .address = "Seattle WA, 11 NW st., apt. 34B",
+        .email = "george.black@gmail.com",
+        .age = 18,
+        .grade = 1,
+        .health = 150.6
+    };
 
-    std::vector<Student> students;
-    students.push_back(george);
-    students.push_back(nataly);
-    students.push_back(robert);
+    Student nataly
+    {
+        .first_name = "Nataly",
+        .last_name = "White",
+        .id = "F-0000-0002",
+        .mobile_phone = "+1(253)535-35-35",
+        .address = "Redmond WA, 52 NS st., apt. 23C",
+        .email = "nataly.rostova@hotmail.com",
+        .age = 19,
+        .grade = 2,
+        .health = 112.4
+    };
+
+    Student robert
+    {
+        .first_name = "Robert",
+        .last_name = "Young",
+        .id = "D-0000-0003",
+        .mobile_phone = "+1(253)453-53-53",
+        .address = "Auburn WA, 92 NW st., apt. 13B",
+        .email = "robert.young@hotmail.com",
+        .age = 20,
+        .grade = 3,
+        .health = 109.8
+    };
+
+    Student donald
+    {
+        .first_name = "Donald",
+        .last_name = "Smith",
+        .id = "C-0000-0004",
+        .mobile_phone = "+1(253)425-25-25",
+        .address = "Kirkland WA, 23 NE st., apt. 33C",
+        .email = "ronald.smith@hotmail.com",
+        .age = 18,
+        .grade = 1,
+        .health = 124.2
+    };
+
+    std::vector<Student> students {george, nataly, robert, donald};
+
     print(students);
-    live(students);
-    students.pop_back();
-    std::cout << "\nr.i.p Robert\n\n"; 
+
+    for (int i = 0; i < 10; ++i)
+    {
+        live(students);
+    }
+
     print(students);
-    live(students);
 }
