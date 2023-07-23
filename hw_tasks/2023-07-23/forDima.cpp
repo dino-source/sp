86587227d98a201e4134b9e93e7ec30e98f796c6
Dino Source <dinosource.cpp@gmail.cpp>
2023-07-23 09:05:54 +0400

    Add new HW task: define Car class

---
+++ hw_tasks/2023-07-23/forDima.cpp
@@ -0,0 +1,25 @@
+/*
+Implement struct Car with the following data members:
+1. brand (for example, "Audi")
+2. model (for example, "A4")
+3. year (for example, 2019)
+4. color (for example, "white")
+5. price (for example, $32000)
+6. tank (you fill up car tank with fuel, so code out connection between 'tank'
+data member and fill_up() member function)
+7. max_speed (for examle, 260 km/h)
+
+Provide the following member functions:
+1. move()
+2. stop()
+3. fill_up()
+4. info()
+
+Use std::cout object to output car status to console. Define std::vector of 4
+objects of type Car. Use range-based 'for' loop to visit each element of the
+vector and output each car info to console.
+Car member functions have to be defined with respect to constness (constant
+member functions are those functions that are denied permission to change the
+values of the data members of their class).
+Use 'if-else' statement where applicable.
+*/
