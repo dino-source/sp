7744f727836077746f916b54dcc5011be44a6be2
Dino Source <dinosource.cpp@gmail.cpp>
2023-07-23 16:07:44 +0400

    Add new HW task: define Dog class

---
+++ hw_tasks/2023-07-23/forNadia.cpp
@@ -0,0 +1,25 @@
+/*
+Implement Dog class with the following data members:
+1. breed (for example, "Siberian Husky")
+2. name (for example, "Wolf")
+3. date_of_birth (for example, "2015-08-25")
+4. fur_color (for example, "white/black")
+5. eyes_color (for example, "blue")
+6. weight (for example, 23 kg)
+7. height (for example, 52 cm)
+
+Provide the following member functions:
+1. eat()
+2. sleep()
+3. play()
+4. sound()
+5. info()
+
+Use std::cout object to output dog status to console. Define std::vector of 4
+objects of type Dog. Use range-based 'for' loop to visit each element of the
+vector and output each dog info to console.
+Dog member functions have to be defined with respect to constness (constant
+member functions are those functions that are denied permission to change the
+values of the data members of their class).
+Use 'if-else' statement where applicable.
+*/
