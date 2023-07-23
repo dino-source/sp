d2a6cb75264c6de442d12b26f31a4e8ef0abe64a
Dino Source <dinosource.cpp@gmail.cpp>
2023-07-23 15:33:20 +0400

    Add hw task: define Cat class

---
+++ hw_tasks/2023-07-23/forLada.cpp
@@ -0,0 +1,25 @@
+/*
+Implement Cat class with the following data members:
+1. breed (for example, "Maine Coon")
+2. name (for example, "Wendy")
+3. date_of_birth (for example, "2016-07-25")
+4. fur_color (for example, "brown")
+5. eyes_color (for example, "green")
+6. weight (for example, 7 kg)
+7. height (for example, 35 cm)
+8. length (for example, 80 cm)
+
+Provide the following member functions:
+1. eat()
+2. sleep()
+3. run()
+4. sound()
+
+Use std::cout object to output cat status to console. Define std::vector of 4
+objects of type Cat. Use range-based 'for' loop to visit each element of the
+vector and output each car info to console.
+Cat member functions have to be defined with respect to constness (constant
+member functions are those functions that are denied permission to change the
+values of the data members of their class).
+Use 'if-else' statement where applicable.
+*/
