6ceb607653a6e029ad3b33f743c7fd2fbbc2bb2b
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-07-24 21:18:33 +0500

    Add while loop example

---
+++ online_sessions/2023-07-24/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char const *argv[])
+{
+    constexpr int LIMIT{10};
+    int s{0};
+    while (s < LIMIT)
+    {
+        cout << s << 
+    }
+    
+    return 0;
+}
