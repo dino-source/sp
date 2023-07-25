169b75b31b9bf3a9bf4d52a85e2694ea482b26b6
Dino Source <dinosource.cpp@gmail.cpp>
2023-07-25 14:24:47 +0400

    Remove unnecessary folder

--- chapter02/exercises/ex04/main.cpp
+++
@@ -1,8 +0,0 @@
-// Stephen Prata's "C++ Primer Plus" (Sixth Edition)
-
-// Chapter 2. Exercise 4.
-
-// Write a program that asks the user to enter his or her age.The program then should
-// display the age in months:
-// Enter your age: 29
-// Your age in months is 384.
