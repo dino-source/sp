20360810557a1befcda3d9e1310f6aaa85cf894e
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-08-03 22:11:40 +0500

    Implement currency converter (draft)

--- online_sessions/2023-08-03/main.cpp
+++ online_sessions/2023-08-03/main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-// 0.91 - usd to eur
-// 93.79 - usd to rub
-// 103.00 - eur to rub
-// 1.09 - eur to usd
+
+constexpr double USD_TO_EUR = 0.91;
+constexpr double USD_TO_RUB = 93.79;
+constexpr double EUR_TO_USD = 1.09;
+constexpr double EUR_TO_RUB = 103.00;
+constexpr double RUB_TO_USD = 0.011;
+constexpr double RUB_TO_EUR = 0.0097;
 
 int main()
 {
     double money_amount{100};
-    
-    const double CURRENCY_USD_TO_EUR{0.91};
-    double eur = money_amount * CURRENCY_USD_TO_EUR;
+
+    double eur = money_amount * USD_TO_EUR;
     std::cout << "Usd to eur: " << eur << "\n";
 
-    const double CURRENCY_USD_TO_RUB{93.79};
-    double rub = money_amount * CURRENCY_USD_TO_RUB;
+    double rub = money_amount * USD_TO_RUB;
     std::cout << "Usd to rub: " << rub << "\n";
 
-    const double CURRENCY_EUR_TO_RUB{103.00};
-    rub = money_amount * CURRENCY_EUR_TO_RUB;
-    std::cout << "Eur to rub: " << rub << "\n"; 
-
+    rub = money_amount * EUR_TO_RUB;
+    std::cout << "Eur to rub: " << rub << "\n";
 }
