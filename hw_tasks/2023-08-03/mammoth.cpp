7a8cab0da8c4a79e53fc50601385bf18fc184496
Dino Source <dinosource.cpp@gmail.cpp>
2023-08-03 23:01:32 +0400

    Add HW tasks

---
+++ hw_tasks/2023-08-03/mammoth.cpp
@@ -0,0 +1,14 @@
+/*
+When mammoth walks through the forest it makes 1 step per 4 seconds, while
+elephant flea walking through the mammoth's body makes 3 steps per 1 second.
+
+When mammoth walks through the tundra it makes 1 step per 3 seconds, while
+elephant flea's speed remains the same.
+
+When mammoth walks through the hills it makes 1 step per 5 seconds, while
+elephant flea's speed increasing: it makes 5 steps per 1 second, because
+elephant flea is very fond of hills.
+
+Print to the terminal how many steps make mammoth and elephant flea together
+during 1 minute in each type of landscape (forest, tundra, hills).
+*/
