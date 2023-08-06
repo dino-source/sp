7b7614e84a7d391bf13be929b9e01644ba414b4c
Yajnadev <dev548913481@gmail.com>
2023-08-06 19:14:47 +0300

    Refactor code using functions and for loop

--- online_sessions/2023-08-06/main.cpp
+++ online_sessions/2023-08-06/main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-void user_prompt(std::string const &msg)
+void user_prompt(std::string const &msg, std::string const &delim)
 {
-    std::cout << msg << '\n';
+    std::cout << msg << delim;
+}
+
+std::string get_string_from_user()
+{
+    std::string userAnswer;
+    std::cin >> userAnswer;
+    return userAnswer;
 }
 
 int main()
 {
-    int w = 88;
-    std::cout << "address of variable w: " << &w << '\n';
-    std::cout << "Please enter your age: ";
-    int userAge = 0;
-    std::cin >> userAge;
-    std::cout << "You just entered: " << userAge << '\n';
-
-    std::cout << "Please enter your name: ";
-    std::string userName;
-    std::cin >> userName;
-    std::cout << "Now I know your name: " << userName << '\n';
+    std::vector<std::string> user_properties{
+        "first name", "last name", "address", "email", "phone"};
+    std::string delimiter = ": ";
+    for (size_t i = 0; i < user_properties.size(); i++)
+    {
+        user_prompt("Please enter your " + user_properties[i], delimiter);
+        std::string user_property = get_string_from_user();
+        std::cout << "Now I know your " + user_properties[i] + delimiter << user_property << '\n';
+    }
 }
