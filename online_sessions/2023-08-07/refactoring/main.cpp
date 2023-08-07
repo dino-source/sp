14eca8315c20fc4350282541c911529d028ec113
Dino Source <dinosource.cpp@gmail.cpp>
2023-08-07 18:03:15 +0400

    FIx intro message in 'refactoring' excercise

--- online_sessions/2023-08-07/refactoring/main.cpp
+++ online_sessions/2023-08-07/refactoring/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     std::cout << "Hello! My name is Rob Ott. I am your virtual psychologist.\n"
                  "The first step we have to do is to fill a questionnaire.\n"
-                 "It'll be our start point. So let's start.\n\n";
+                 "It'll be the starting point in your therapy. So let's start.\n\n";
 
     std::cout << "Please enter your first name: ";
     std::string first_name;
