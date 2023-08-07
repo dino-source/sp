41075ee11622452b3c78c0ff331ff7cc96dfaf99
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-08-07 22:08:30 +0500

    Define print function to get rid of hardcoded literals

--- online_sessions/2023-08-07/refactoring/main.cpp
+++ online_sessions/2023-08-07/refactoring/main.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+void print(std::string const &patient_data, std::string const &delim);
 
 int main()
 {
+    std::vector<std::string> patient_properties{
+        "first name", "last name", "ID number", "phone number",
+        "email", "occupation", "marital status", "date of birth"};
+    std::string delim = ": ";
     std::cout << "Hello! My name is Rob Ott. I am your virtual psychologist.\n"
                  "The first step we have to do is to fill a questionnaire.\n"
                  "It'll be a starting point in your therapy. So let's start.\n\n";
 
-    std::cout << "Please enter your first name: ";
+    print(patient_properties[0], delim);
     std::string first_name;
     std::cin >> first_name;
 
-    std::cout << "Please enter your last name: ";
+    print(patient_properties[1], delim);
     std::string last_name;
     std::cin >> last_name;
 
-    std::cout << "Please enter your ID number: ";
+    print(patient_properties[2], delim);
     std::string id_number;
     std::cin >> id_number;
 
-    std::cout << "Please enter your phone number: ";
+    print(patient_properties[3], delim);
     std::string phone;
     std::cin >> phone;
 
-    std::cout << "Please enter your email: ";
+    print(patient_properties[4], delim);
     std::string email;
     std::cin >> email;
 
-    std::cout << "Please enter your occupation: ";
+    print(patient_properties[5], delim);
     std::string occupation;
     std::cin >> occupation;
 
-    std::cout << "Please enter your marital status: ";
+    print(patient_properties[6], delim);
     std::string marital_status;
     std::cin >> marital_status;
 
-    std::cout << "Please enter your date of birth: ";
+    print(patient_properties[7], delim);
     std::string date_of_birth;
     std::cin >> date_of_birth;
+}
+
+void print(std::string const &patient_data, std::string const &delim)
+{
+    std::cout << "Please enter your " << patient_data << delim;
 }
