6dd280751819e956188034f98c0b5c5f99229ccc
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-08-07 22:49:06 +0500

    Define get_string_from_user to get rid of hardcoded std::cin

--- online_sessions/2023-08-07/refactoring/main.cpp
+++ online_sessions/2023-08-07/refactoring/main.cpp
@@ -3,51 +3,60 @@
 #include <vector>
 
 void print(std::string const &patient_data, std::string const &delim);
+std::string get_string_from_user();
 
 int main()
 {
     std::vector<std::string> patient_properties{
         "first name", "last name", "ID number", "phone number",
         "email", "occupation", "marital status", "date of birth"};
+    std::vector<std::string> patient_data;
     std::string delim = ": ";
     std::cout << "Hello! My name is Rob Ott. I am your virtual psychologist.\n"
                  "The first step we have to do is to fill a questionnaire.\n"
                  "It'll be a starting point in your therapy. So let's start.\n\n";
 
     print(patient_properties[0], delim);
-    std::string first_name;
-    std::cin >> first_name;
+    std::string user_input = get_string_from_user();
+    patient_data.push_back(user_input);
 
     print(patient_properties[1], delim);
-    std::string last_name;
-    std::cin >> last_name;
+    user_input = get_string_from_user();
+    patient_data.push_back(user_input);
 
     print(patient_properties[2], delim);
-    std::string id_number;
-    std::cin >> id_number;
+    user_input = get_string_from_user();
+    patient_data.push_back(user_input);
 
     print(patient_properties[3], delim);
-    std::string phone;
-    std::cin >> phone;
+    user_input = get_string_from_user();
+    patient_data.push_back(user_input);
 
     print(patient_properties[4], delim);
-    std::string email;
-    std::cin >> email;
+    user_input = get_string_from_user();
+    patient_data.push_back(user_input);
 
     print(patient_properties[5], delim);
-    std::string occupation;
-    std::cin >> occupation;
+    user_input = get_string_from_user();
+    patient_data.push_back(user_input);
 
     print(patient_properties[6], delim);
-    std::string marital_status;
-    std::cin >> marital_status;
+    user_input = get_string_from_user();
+    patient_data.push_back(user_input);
 
     print(patient_properties[7], delim);
-    std::string date_of_birth;
-    std::cin >> date_of_birth;
+    user_input = get_string_from_user();
+    patient_data.push_back(user_input);
 }
 
 void print(std::string const &patient_data, std::string const &delim)
 {
     std::cout << "Please enter your " << patient_data << delim;
+}
+
+std::string get_string_from_user()
+{
+    std::string user_input;
+    std::cin >> user_input;
+    return user_input;
 }
