c8fa2c0dd31378cb7d5a3551a069ffea7f35e106
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-08-07 23:11:13 +0500

    Define get_patient_data function to clean up main()

--- online_sessions/2023-08-07/refactoring/main.cpp
+++ online_sessions/2023-08-07/refactoring/main.cpp
@@ -4,49 +4,19 @@
 
 void print(std::string const &patient_data, std::string const &delim);
 std::string get_string_from_user();
+using PatientData = std::vector<std::string>;
+PatientData get_patient_data(PatientData &patient_properties);
 
 int main()
 {
     std::vector<std::string> patient_properties{
         "first name", "last name", "ID number", "phone number",
         "email", "occupation", "marital status", "date of birth"};
-    std::vector<std::string> patient_data;
     std::string delim = ": ";
     std::cout << "Hello! My name is Rob Ott. I am your virtual psychologist.\n"
                  "The first step we have to do is to fill a questionnaire.\n"
                  "It'll be a starting point in your therapy. So let's start.\n\n";
-
-    print(patient_properties[0], delim);
-    std::string user_input = get_string_from_user();
-    patient_data.push_back(user_input);
-
-    print(patient_properties[1], delim);
-    user_input = get_string_from_user();
-    patient_data.push_back(user_input);
-
-    print(patient_properties[2], delim);
-    user_input = get_string_from_user();
-    patient_data.push_back(user_input);
-
-    print(patient_properties[3], delim);
-    user_input = get_string_from_user();
-    patient_data.push_back(user_input);
-
-    print(patient_properties[4], delim);
-    user_input = get_string_from_user();
-    patient_data.push_back(user_input);
-
-    print(patient_properties[5], delim);
-    user_input = get_string_from_user();
-    patient_data.push_back(user_input);
-
-    print(patient_properties[6], delim);
-    user_input = get_string_from_user();
-    patient_data.push_back(user_input);
-
-    print(patient_properties[7], delim);
-    user_input = get_string_from_user();
-    patient_data.push_back(user_input);
+    PatientData patient_data = get_patient_data(patient_properties);
 }
 
 void print(std::string const &patient_data, std::string const &delim)
@@ -59,4 +29,17 @@ std::string get_string_from_user()
     std::string user_input;
     std::cin >> user_input;
     return user_input;
+}
+
+PatientData get_patient_data(PatientData &patient_properties)
+{
+    PatientData patient_data;
+    std::string delim = ": ";
+    for (size_t i = 0; i < patient_properties.size(); i++)
+    {
+        print(patient_properties[i], delim);
+        std::string user_input = get_string_from_user();
+        patient_data.push_back(user_input);
+    }
+    return patient_data;
 }
