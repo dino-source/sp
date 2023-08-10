e630ab028805f0f0bc418bd993734cd03bf7d56e
Pesna <prostopesna74@gmail.com>
2023-08-10 20:49:09 +0400

    Use separate compilation

--- online_sessions/therapy_session/helper_functions.cpp
+++ online_sessions/therapy_session/helper_functions.cpp
@@ -1,1 +1,31 @@
-// TODO: Implement this
+#include "helper_functions.h"
+
+void print(std::string const &patient_data, std::string const &delim)
+{
+    std::cout << "Please enter your " << patient_data << delim;
+}
+
+void print(std::string const &message)
+{
+    std::cout << message;
+}
+
+std::string get_string_from_user()
+{
+    std::string user_input;
+    std::cin >> user_input;
+    return user_input;
+}
+
+PatientData get_patient_data(PatientData &patient_properties)
+{
+    PatientData patient_data;
+    std::string delim = ": ";
+    for (size_t i = 0; i < patient_properties.size(); i++)
+    {
+        print(patient_properties[i], delim);
+        std::string user_input = get_string_from_user();
+        patient_data.push_back(user_input);
+    }
+    return patient_data;
+}
--- online_sessions/therapy_session/helper_functions.h
+++ online_sessions/therapy_session/helper_functions.h
@@ -1,1 +1,10 @@
-// TODO: Implement this
+#include <iostream>
+#include <string>
+#include <vector>
+
+void print(std::string const &patient_data, std::string const &delim);
+void print(std::string const &message);
+std::string get_string_from_user();
+using PatientData = std::vector<std::string>;
+using PatientProperties = std::vector<std::string>;
+PatientData get_patient_data(PatientData &patient_properties);
