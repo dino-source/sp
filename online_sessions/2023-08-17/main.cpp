48e2a47b1d5c4b4b55d1894fcd3e2dd409c89f5f
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-08-17 22:22:45 +0500

    Add test to show how logical OR and logical AND work

--- online_sessions/2023-08-17/main.cpp
+++ online_sessions/2023-08-17/main.cpp
@@ -11,9 +11,26 @@ bool is_multiply_by(int n, int m);
 
 int main()
 {
-    int x = 9;
-    int y = 3;
-    std::cout << std::boolalpha << is_multiply_by(x, y) << '\n';
+    int x = 15;
+    int y = 4;
+    std::cout << "Please enter an integer: ";
+    int n;
+    std::cin >> n;
+    if ((is_positive(n) && is_even(n)) || (is_positive(n) && is_multiply_by(n, 5)))
+    {
+        std::cout << ":>\n";
+    }
+    else
+    {
+        if (is_negative(n) || is_zero(n))
+        {
+            std::cout << ":<\n";
+        }
+        else
+        {
+            std::cout << "0_0\n";
+        }
+    }
 }
 
 bool is_even(int n)
