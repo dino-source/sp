d31737d8e3bf441c84e55a5d427dc35c004efaac
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-08-21 00:49:53 +0500

    Solve problem ex05 chapter03, made draft of ex04

--- chapter03/exercises/ex04/main.cpp
+++ chapter03/exercises/ex04/main.cpp
@@ -15,3 +15,23 @@ Enter the number of seconds: 31600000
 затем отображает эквивалентное значение в сутках, часах, минутах и секундах.
 Для представления количества часов в сутках, количества минут в часе и ко­личества секунд
 в минуте используйте символические константы.*/
+
+#include <iostream>
+
+int main()
+{
+    const int seconds_in_one_minute = 60; // 60 seconds in one minute
+    const int minutes_in_one_hour = 60;
+    const int hours_in_one_day = 24;
+    int seconds_in_one_hour = seconds_in_one_minute * minutes_in_one_hour;                   // 3600
+    int seconds_in_one_day = seconds_in_one_minute * minutes_in_one_hour * hours_in_one_day; // 86400
+
+    std::cout << "Enter the number of seconds: ";
+    long num_of_sec;
+    std::cin >> num_of_sec;
+    int days = num_of_sec / seconds_in_one_day;
+    int seconds_in_those_days = days * seconds_in_one_day;
+    int hours = (num_of_sec - seconds_in_those_days) / seconds_in_one_hour;
+    std::cout << num_of_sec << " seconds = " << days << " days,\n"
+              << hours << " hours,\n";
+}
