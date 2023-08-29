fff3345382b5eadcfeb69536a8a30a77be8e57e0
Dino Source <dinosource.cpp@gmail.cpp>
2023-08-29 22:05:34 +0400

    Add small fixes

--- chapter04/online_session/ptr_and_ref.h
+++ chapter04/online_session/ptr_and_ref.h
@@ -8,18 +8,18 @@ void play_with_ptr_and_ref();
 inline void play_with_ptr_and_ref()
 {
     int fruit = 9;
-    int *p_fruit = &fruit;
+    int *ptr_fruit = &fruit;
 
     std::cout << "fruit: " << fruit << '\n';
     std::cout << "&fruit: " << &fruit << '\n';
 
-    std::cout << "*p_fruit: " << *p_fruit << '\n';
-    std::cout << "&p_fruit: " << &p_fruit << '\n';
-    std::cout << "p_fruit: " << p_fruit << '\n';
+    std::cout << "*ptr_fruit: " << *ptr_fruit << '\n';
+    std::cout << "&ptr_fruit: " << &ptr_fruit << '\n';
+    std::cout << "ptr_fruit: " << ptr_fruit << '\n';
 
-    int &r_fruit = fruit;
-    std::cout << "r_fruit: " << r_fruit << '\n';
-    std::cout << "&r_fruit: " << &r_fruit << '\n';
+    int &ref_fruit = fruit;
+    std::cout << "ref_fruit: " << ref_fruit << '\n';
+    std::cout << "&ref_fruit: " << &ref_fruit << '\n';
 
     int apple = 1;
     int banana = 2;
@@ -28,9 +28,9 @@ inline void play_with_ptr_and_ref()
     p_fruits.push_back(&apple);
     p_fruits.push_back(&banana);
     p_fruits.push_back(&peach);
-    for (auto const &ptr_fruit : p_fruits)
+    for (auto const p_fruit : p_fruits)
     {
-        std::cout << ptr_fruit << '\n';
+        std::cout << *p_fruit << '\n';
     }
 }
 #endif // PTR_AND_REF_H
