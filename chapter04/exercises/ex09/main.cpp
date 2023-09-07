605fd8430c0cc92c022266da32cc97659d15f275
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-09-07 21:28:36 +0500

    Add solution of ex09

--- chapter04/exercises/ex09/main.cpp
+++ chapter04/exercises/ex09/main.cpp
@@ -13,5 +13,20 @@ struct CandyBar
 
 int main()
 {
-    CandyBar *dessert = new CandyBar[3];
-} // I don't understand how to do it at all :/
+    std::cout << "Enter the number of desserts: ";
+    unsigned num_of_desserts;
+    std::cin >> num_of_desserts;
+    CandyBar *dessert = new CandyBar[num_of_desserts];
+
+    *(dessert + 0) = {"Tiramisu", 200.5, 566};
+    *(dessert + 1) = {"Brownie", 50.8, 233};
+    *(dessert + 2) = {"Chocho Pudding", 70.5, 271};
+
+    std::cout << dessert[0].name << ", " << dessert[0].weight << ", "
+              << dessert[0].calories << ".\n";
+    std::cout << dessert[1].name << ", " << dessert[1].weight << ", "
+              << dessert[1].calories << ".\n";
+    std::cout << dessert[2].name << ", " << dessert[2].weight << ", "
+              << dessert[2].calories << ".\n";
+    delete[] dessert;
+}
