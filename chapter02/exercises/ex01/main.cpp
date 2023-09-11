2579394ad2721dafb0c35cc1e02c8520f268fa6c
Dino Source <dinosource.cpp@gmail.cpp>
2023-09-11 16:39:59 +0400

    Add solved problems from chapter02 to test hw-framework

---
+++ chapter02/exercises/ex01/main.cpp
@@ -0,0 +1,13 @@
+// Stephen Prata's "C++ Primer Plus" (Sixth Edition)
+
+// Chapter 2. Exercise 1.
+
+// Write a C++ program that displays your name and address (or if you value your
+// privacy, a fictitious name and address).
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Name: Lada\nAddress: Lemon St.11\n";
+}
---
+++ chapter02/exercises/ex03/main.cpp
@@ -0,0 +1,38 @@
+// Stephen Prata's "C++ Primer Plus" (Sixth Edition)
+
+// Chapter 2. Exercise 3.
+
+// Write a C++ program that uses three user-defined functions (counting main() as
+// one) and produces the following output:
+// Three blind mice
+// Three blind mice
+// See how they run
+// See how they run
+// One function, called two times, should produce the first two lines, and the remain-
+// ing function, also called twice, should produce the remaining output.
+
+#include <iostream>
+
+void three_blind_mice(); // function prototype
+void see_how_they_run(); // function prototype
+
+int main ()
+{
+    three_blind_mice();
+    three_blind_mice();
+    see_how_they_run();
+    see_how_they_run();
+}
+
+
+void three_blind_mice()
+{
+    using namespace std;
+    cout << "Three blind mice\n";
+}
+
+void see_how_they_run()
+{
+    using namespace std;
+    cout << "See how they run\n";
+}
