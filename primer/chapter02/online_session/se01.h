9db5fac643a184bc782f7d9f5968468a0aa5773a
Dino Source <dinosource.cpp@gmail.cpp>
2023-09-14 11:34:49 +0400

    Add ch02 and ch03 online_sessions

---
+++ primer/chapter02/online_session/se01.h
@@ -0,0 +1,14 @@
+#ifndef CHAPTER_02_SIMPLE_EXAMPLE_01_H
+#define CHAPTER_02_SIMPLE_EXAMPLE_01_H
+
+#include <iostream>
+
+namespace ch02::se01
+{
+    inline void play_with_input_and_output()
+    {
+        std::cout << "Chapter 2. Simple example 1. TODO: implement it\n";
+    }
+} // namespace ch02::se01
+
+#endif // CHAPTER_02_SIMPLE_EXAMPLE_01_H
---
+++ primer/chapter03/online_session/se01.h
@@ -0,0 +1,14 @@
+#ifndef CHAPTER_03_SIMPLE_EXAMPLE_01_H
+#define CHAPTER_03_SIMPLE_EXAMPLE_01_H
+
+#include <iostream>
+
+namespace ch03::se01
+{
+    inline void play_with_conversions()
+    {
+        std::cout << "Chapter 3. Simple example 1. TODO: implement it\n";
+    }
+} // namespace ch03::se01
+
+#endif // CHAPTER_03_SIMPLE_EXAMPLE_01_H
