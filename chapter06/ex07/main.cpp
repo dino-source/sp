aeb8b590737b9c0c6bad7dba52c62d1726e75040
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-09-26 19:39:45 +0500

    Add ex07 chapter06

--- chapter06/ex07/main.cpp
+++ chapter06/ex07/main.cpp
@@ -7,7 +7,7 @@
 switch для идентификации тех слов, прошедших проверку isalpha(), кото­рые начинаются
 с гласных. Пример запуска может выглядеть так:
 Enter words (q to quit):
-The 12 awesome oxen am bled
+The 12 awesome oxen ambled
 quietly across 15 meters of lawn. q
 5 words begining with vowels
 4 words begining with consonants
@@ -17,38 +17,54 @@ quietly across 15 meters of lawn. q
 #include <cctype>
 #include <string>
 
+bool is_vowel(char ch);
+bool is_consonant(char ch);
+bool starts_from_vowel(const std::string &word);
+bool starts_from_consonant(const std::string &word);
+
 int main()
 {
     std::cout << "Enter words (q to quit): ";
-    std::string word;
     int vowels{};
     int consonants{};
     int others{};
-    while (std::cin >> word && word != "q")
+    for (std::string word; std::cin >> word && word != "q";)
     {
-        if (isalpha(word[0]))
+        if (starts_from_vowel(word))
         {
-            switch (word[0])
-            {
-            case /* constant-expression */:
-                /* code */
-                break;
-
-            default:
-                break;
-            }
-            else
-            {
-                consonants++;
-            }
+            ++vowels;
+        }
+        else if (starts_from_consonant(word))
+        {
+            ++consonants;
         }
         else
         {
-            others++;
+            ++others;
         }
-        std::cin >> (word);
     }
-    std::cout << vowels << " words begining with vowels "
-              << consonants << " words begining with consonants "
+    std::cout << vowels << " words begining with vowels;\n"
+              << consonants << " words begining with consonants;\n"
               << others << " others.\n";
+}
+
+bool is_vowel(char ch)
+{
+    ch = tolower(ch);
+    return std::string("aeiou").contains(ch);
+}
+
+bool is_consonant(char ch)
+{
+    return isalpha(ch) && !is_vowel(ch);
+}
+
+bool starts_from_vowel(const std::string &word)
+{
+    return is_vowel(word.front());
+}
+
+bool starts_from_consonant(const std::string &word)
+{
+    return is_consonant(word.front());
 }
