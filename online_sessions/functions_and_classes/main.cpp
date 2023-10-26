32dee028822c1a5176f6603b93bbeb7ba490f198
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-10-26 18:54:54 +0500

    Add least/most_significant_digit functions

--- online_sessions/functions_and_classes/main.cpp
+++ online_sessions/functions_and_classes/main.cpp
@@ -3,13 +3,18 @@
 #include <algorithm>
 
 void reverse(std::string &s);
+int least_significant_digit(int n);
+int most_significant_digit(int n);
 
 int main()
 {
-    std::string text("redruma");
-    std::cout << "Before: " << text;
-    reverse(text);
-    std::cout << "\nAfter: " << text << '\n';
+    int x{29};
+    int y{438};
+    int z{7956};
+    int w{-1249};
+    int result = least_significant_digit(w);
+    std::cout << result << '\n';
+
 }
 
 void reverse(std::string &s)
@@ -18,4 +23,36 @@ void reverse(std::string &s)
     {
         std::swap(s[i], s[k]);
     }
+    return;
+}
+
+int least_significant_digit(int n)
+{
+    int result{-1};
+    result = n%10;
+
+    return result >= 0 ? result : -result;
+}
+
+int most_significant_digit(int n)
+{
+    int result{-1};
+
+    if (n > -10 && n < 10)
+    {
+        result = n;
+    }
+    else if (n > -100 && n < 100)
+    {
+        result = n/10;
+    }
+    else if (n > -1000 && n < 1000)
+    {
+        result = n/100;
+    }
+    else if (n > -10000 && n < 10000)
+    {
+        result = n/1000;
+    }
+    return result >= 0 ? result : -result;
 }
