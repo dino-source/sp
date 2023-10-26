149e3dc0f1f39ba941740c92d4646a0b63e25e64
Mint Marshmellow <mint.marshmellow94@gmail.com>
2023-10-26 20:13:33 +0500

    Play with functions

--- online_sessions/functions_and_classes/main.cpp
+++ online_sessions/functions_and_classes/main.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
+#include <functional>
 
 void reverse(std::string &s);
 int least_significant_digit(int n);
 int most_significant_digit(int n);
+void reverse(int &n);
+void compute_number_of_steps(int &n);
+bool is_greater_than_and_even(int a, int b);
+bool is_less_than_and_even(int a, int b); // doesn't work
 
 int main()
 {
-    int x{29};
-    int y{438};
-    int z{7956};
-    int w{-1249};
-    int result = least_significant_digit(w);
-    std::cout << result << '\n';
-
+    std::vector<int> numbers{1, 48, 92, 7, 13, 5, 9};
+    auto is_less_than_and_odd = [](int a, int b){return a<b && a%2;};
+    std::sort(numbers.begin(), numbers.end(), is_less_than_and_odd);
+    for (auto i : numbers)
+    {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
 }
 
 void reverse(std::string &s)
 {
+    int number_of_steps{0};
+
     for (size_t i = 0, k = s.size()-1; i < s.size()/2; i++, k--)
     {
         std::swap(s[i], s[k]);
+        compute_number_of_steps(number_of_steps);
     }
-    return;
+    std::cout << "Total number of steps: " << number_of_steps << '\n';
+    return ;
 }
 
 int least_significant_digit(int n)
@@ -55,4 +66,27 @@ int most_significant_digit(int n)
         result = n/1000;
     }
     return result >= 0 ? result : -result;
+}
+
+void reverse(int &number)
+{
+    std::string number_as_string{std::to_string(number)};
+    reverse(number_as_string);
+    number = std::stoi(number_as_string); 
+}
+
+void compute_number_of_steps(int &n)
+{
+    n++;
+}
+
+
+bool is_greater_than_and_even(int a, int b)
+{
+    return a>b && !(a%2);
+}
+
+bool is_less_than_and_even(int a, int b)
+{
+    return a<b && !(a%2);
 }
